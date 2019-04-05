42fb6bdffcaea35ee7f4d57303deceae8b11cbbc
Jessie Jones <jessiejonesofficial@gmail.com>
2019-04-04 23:29:03 -0600

    moved api keys to .env.  Added gitignore

--- demo.py
+++ demo.py
@@ -1,11 +1,7 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'kVmGSnXxOqZbE4NkJM5xxXH0c' 
-consumer_secret = 'UiFY5oIgPaaJ5bX3C5b7AFqUDSPYcVhRDua7lw1WmRoIM1vycw'
 
-access_token = '1114013036069527552-p7aBhtEFkb8mDx7nch21pA36TC0V4P'
-acces_token_secret = 'gcTUIY8dHxgjrEFfqxwvb4SqADRO68qHnUD1jBjhUwhgX'
 
 #user auth
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
